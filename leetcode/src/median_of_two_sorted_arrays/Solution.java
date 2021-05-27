f397185e48f41a337806ba91f2b1edae3ddeae22
Kenneth Xu <kenneth@homexu.com>
2021-05-27 14:15:22 -0400

    Minor cleanup for leetcode 'median of two sorted array'

--- leetcode/src/median_of_two_sorted_arrays/Solution.java
+++ leetcode/src/median_of_two_sorted_arrays/Solution.java
@@ -1,34 +1,34 @@
 package median_of_two_sorted_arrays;
 
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        final int[] s = nums1.length <= nums2.length ? nums1 : nums2; // the shorter array
-        final int[] l = s == nums1 ? nums2 : nums1; // the longer array
-        final int size = s.length + l.length;
-        final boolean odd = (size % 2 != 0);
+    public double findMedianSortedArrays(final int[] nums1, final int[] nums2) {
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1); // ensure nums1 is shorter;
 
-        int start = 0, end = s.length-1, mid = (size - 1) / 2;
+        final int n = nums1.length + nums2.length;
+        final boolean odd = (n % 2 != 0);
+
+        int start = 0, end = nums1.length-1, mid = (n - 1) / 2;
         if (end < 0) { // one of the array is empty
-            return odd ? l[mid] : (l[mid] + l[mid+1]) / 2.0;
+            return odd ? nums2[mid] : (nums2[mid] + nums2[mid+1]) / 2.0;
         }
-        if (s[0] >= l[mid]) { // no element in s is less than any element in l
-            return odd ? l[mid] : (l[mid] + (mid == end ? s[0] : Math.min(l[mid+1], s[0]))) / 2.0;
+        if (nums1[0] >= nums2[mid]) { // no element in nums1 is less than any element in nums2
+            return odd ? nums2[mid] : (nums2[mid] + (mid == end ? nums1[0] : Math.min(nums2[mid+1], nums1[0]))) / 2.0;
         }
-        mid = size / 2 - s.length;
-        if (s[end] <= l[mid]) { // no element in s is larger than any element in l
-            return odd ? l[mid] : (l[mid] + (mid == 0 ? s[end] : Math.max(l[mid-1], s[end]))) / 2.0;
+        mid = n / 2 - nums1.length;
+        if (nums1[end] <= nums2[mid]) { // no element in nums1 is larger than any element in nums2
+            return odd ? nums2[mid] : (nums2[mid] + (mid == 0 ? nums1[end] : Math.max(nums2[mid-1], nums1[end]))) / 2.0;
         }
         for (;;) {
-            mid = (end - start) / 2 + start;
-            int index = (size-1) / 2 - mid - 1;
-            if (l[index] <= s[mid] && l[index+1] >= s[mid]) {
-                return odd ? s[mid] : (s[mid] + Math.min(s[mid+1], l[index+1])) / 2.0;
+            int mid1 = (end + start) / 2;
+            int mid2 = (n - 1) / 2 - mid1 - 1;
+            if (nums2[mid2] <= nums1[mid1] && nums2[mid2+1] >= nums1[mid1]) {
+                return odd ? nums1[mid1] : (nums1[mid1] + Math.min(nums1[mid1+1], nums2[mid2+1])) / 2.0;
             }
-            if (s[mid] <= l[index] && s[mid+1] >= l[index]) {
-                return odd ? l[index] : (l[index] + Math.min(s[mid+1], l[index+1])) / 2.0;
+            if (nums1[mid1] <= nums2[mid2] && nums1[mid1+1] >= nums2[mid2]) {
+                return odd ? nums2[mid2] : (nums2[mid2] + Math.min(nums1[mid1+1], nums2[mid2+1])) / 2.0;
             }
-            if (l[index] > s[mid]) start = mid + 1;
-            else end = mid - 1;
+            if (nums2[mid2] > nums1[mid1]) start = mid1 + 1;
+            else end = mid1 - 1;
         }
     }
 }
--- leetcode/src/median_of_two_sorted_arrays/Solution2.java
+++ leetcode/src/median_of_two_sorted_arrays/Solution2.java
@@ -4,22 +4,22 @@ class Solution2 {
     public double findMedianSortedArrays(final int[] nums1, final int[] nums2) {
         final int n1d = nums1.length * 2;
         final int n2d = nums2.length * 2;
-        if (n1d > n2d) return findMedianSortedArrays(nums2, nums1);	// Ensure nums1 is shorter.
+        if (n1d > n2d) return findMedianSortedArrays(nums2, nums1);	// ensure nums1 is shorter.
 
         final int n = (n1d + n2d) / 2;
         int start = 0, end = n1d;
         for (;;) {
-            int mid1 = (start + end) / 2;   // binary cut nums1
-            int mid2 = n - mid1;  // corresponding but point for nums2
+            int mid1 = (start + end) / 2; // binary cut nums1
+            int mid2 = n - mid1;  // corresponding cut point for nums2
 
             int left1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[(mid1-1)/2];
             int left2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[(mid2-1)/2];
             int right1 = (mid1 == n1d) ? Integer.MAX_VALUE : nums1[mid1/2];
             int right2 = (mid2 == n2d) ? Integer.MAX_VALUE : nums2[mid2/2];
 
-            if (right1 < left2) start = mid1 + 1;		// nums1's upper half is too small;
-            else if (left1 > right2) end = mid1 - 1;	// nums1's lower half is too big;
-            else return (Math.max(left1,left2) + Math.min(right1, right2)) / 2.0;
+            if (right1 < left2) start = mid1 + 1;    // nums1's upper half is too small;
+            else if (left1 > right2) end = mid1 - 1; // nums1's lower half is too big;
+            else return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
         }
     }
 }
