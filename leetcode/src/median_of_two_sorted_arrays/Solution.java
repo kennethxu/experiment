8292d9b03efbfd8cdb35cec964c9e6d9e09d74af
Kenneth Xu <kenneth@homexu.com>
2021-05-27 16:37:45 -0400

    leetcode: simplify the solution code for 'median of two sorted arrays' problem

--- leetcode/src/median_of_two_sorted_arrays/Solution.java
+++ leetcode/src/median_of_two_sorted_arrays/Solution.java
@@ -5,30 +5,22 @@ class Solution {
         if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1); // ensure nums1 is shorter;
 
         final int n = nums1.length + nums2.length;
-        final boolean odd = (n % 2 != 0);
+        final int r1 = nums1.length - 1, r2 = nums2.length - 1;
 
-        int start = 0, end = nums1.length-1, mid = (n - 1) / 2;
-        if (end < 0) { // one of the array is empty
-            return odd ? nums2[mid] : (nums2[mid] + nums2[mid+1]) / 2.0;
-        }
-        if (nums1[0] >= nums2[mid]) { // no element in nums1 is less than any element in nums2
-            return odd ? nums2[mid] : (nums2[mid] + (mid == end ? nums1[0] : Math.min(nums2[mid+1], nums1[0]))) / 2.0;
-        }
-        mid = n / 2 - nums1.length;
-        if (nums1[end] <= nums2[mid]) { // no element in nums1 is larger than any element in nums2
-            return odd ? nums2[mid] : (nums2[mid] + (mid == 0 ? nums1[end] : Math.max(nums2[mid-1], nums1[end]))) / 2.0;
-        }
-        for (;;) {
-            int mid1 = (end + start) / 2;
+        for (int start = 0, end = r1;;) {
+            int mid1 = (end + start + 2) / 2 - 1;
             int mid2 = (n - 1) / 2 - mid1 - 1;
-            if (nums2[mid2] <= nums1[mid1] && nums2[mid2+1] >= nums1[mid1]) {
-                return odd ? nums1[mid1] : (nums1[mid1] + Math.min(nums1[mid1+1], nums2[mid2+1])) / 2.0;
-            }
-            if (nums1[mid1] <= nums2[mid2] && nums1[mid1+1] >= nums2[mid2]) {
-                return odd ? nums2[mid2] : (nums2[mid2] + Math.min(nums1[mid1+1], nums2[mid2+1])) / 2.0;
-            }
-            if (nums2[mid2] > nums1[mid1]) start = mid1 + 1;
-            else end = mid1 - 1;
+
+            int left1 = mid1 < 0 ? Integer.MIN_VALUE : nums1[mid1];
+            int left2 = mid2 < 0 ? Integer.MIN_VALUE : nums2[mid2];
+            int right1 = mid1 == r1 ? Integer.MAX_VALUE : nums1[mid1 + 1];
+            int right2 = mid2 == r2 ? Integer.MAX_VALUE : nums2[mid2 + 1];
+
+            if (left1 > right2) end = mid1 - 1;
+            else if (right1 < left2) start = mid1 + 1;
+            else return (n % 2 != 0) ?
+                        Math.max(left1, left2) :
+                        (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
         }
     }
 }
